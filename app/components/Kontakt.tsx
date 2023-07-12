ec797b25d510e51a0ffc3fd54886f0b9ea6b861e
Umut Akbayin <umutakbayin@posteo.de>
2023-07-12 21:07:31 +0200

    add nodemailer and react-hook-form

--- app/components/Kontakt.tsx
+++ app/components/Kontakt.tsx
@@ -2,7 +2,6 @@
 
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import InputLabel from '@mui/material/InputLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
@@ -32,43 +31,45 @@ function Kontakt() {
         <Typography variant="body2">
           *Pflichtfelder
         </Typography>
-        <TextField
-          fullWidth
-          required
-          id="outlined-basic"
-          label="Vorname"
-          variant="outlined"
-        />
-        <TextField
-          fullWidth
-          id="outlined-basic"
-          label="Nachname"
-          variant="outlined"
-        />
-        <TextField
-          required
-          fullWidth
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-        />
-        <TextField
-          required
-          fullWidth
-          id="outlined-basic"
-          label="Telefon"
-          variant="outlined"
-        />
-        <TextField
-          required
-          fullWidth
-          multiline
-          minRows={3}
-          id="outlined-basic"
-          label="Nachricht"
-          variant="outlined"
-        />
-        <Button variant="outlined">Senden</Button>
+        <form>
+          <TextField
+            fullWidth
+            required
+            id="outlined-basic"
+            label="Vorname"
+            variant="outlined"
+          />
+          <TextField
+            fullWidth
+            id="outlined-basic"
+            label="Nachname"
+            variant="outlined"
+          />
+          <TextField
+            required
+            fullWidth
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+          />
+          <TextField
+            required
+            fullWidth
+            id="outlined-basic"
+            label="Telefon"
+            variant="outlined"
+          />
+          <TextField
+            required
+            fullWidth
+            multiline
+            minRows={3}
+            id="outlined-basic"
+            label="Nachricht"
+            variant="outlined"
+          />
+          <Button variant="outlined">Senden</Button>
+        </form>
       </Container>
     </Container>
   )
